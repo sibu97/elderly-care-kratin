4118be1fdc02ac5e7a79bece74131f873fd98d95
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-04-06 23:45:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Bmi from './components/Bmi/Bmi';
+import Diet from './components/Diet/Diet';
+import Entertainment from './components/Entertainment/Entertainment';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Yoga from './components/Yoga/Yoga';
+
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/entertainment" element={<Entertainment />} />
+        <Route exact path="/diet" element={<Diet />} />
+        <Route exact path="/yoga" element={<Yoga />} />
+        <Route exact path="/bmi" element={<Bmi />} />
+      </Routes>
+    </>
   );
 }
 
