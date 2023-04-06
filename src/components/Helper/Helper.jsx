915df6318774d02e4871273df55ce43e1df67937
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-04-07 00:21:58 +0530

    Initial commit

--- src/components/Helper/Helper.jsx
+++ src/components/Helper/Helper.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Helper = () => {
     return (
         <div className='sticky top-0 bg-green-500 shadow-md  w-full p-4 text-center -z-20'>
-            <p>@chetan6780</p>
+            <p>@sibananda</p>
         </div>
     )
 }
